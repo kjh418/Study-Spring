4fd9c235768b2126cf18ac5512cd4798174d43c6
kjh <devkjh418@gmail.com>
2024-06-24 22:34:37 +0900

    #42 - 시큐리티 설정과 자바 대응 코드 구현
    스프링 부트 2.7 과 가이드에 맞춰서 시큐리티 설정을 적용하고, 인증 정보를 db로부터 부르는 빈과 패스워드 인코더 빈까지 등록
    영향 받는 부분을 서비스 코드와 컨트롤러에 반영

--- project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -1,21 +1,31 @@
 package com.fastcampus.projectboard.repository;
-import com.fastcampus.projectboard.config.JpaConfig;
+
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.UserAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 import java.util.List;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 @DisplayName("JPA 연결 테스트")
-@Import(JpaConfig.class)
+@Import(JpaRepositoryTest.TestJpaConfig.class)
 @DataJpaTest
 class JpaRepositoryTest {
+
     private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
     private final UserAccountRepository userAccountRepository;
+
     public JpaRepositoryTest(
             @Autowired ArticleRepository articleRepository,
             @Autowired ArticleCommentRepository articleCommentRepository,
@@ -25,30 +35,36 @@ class JpaRepositoryTest {
         this.articleCommentRepository = articleCommentRepository;
         this.userAccountRepository = userAccountRepository;
     }
+
     @DisplayName("select 테스트")
     @Test
     void givenTestData_whenSelecting_thenWorksFine() {
         // Given
+
         // When
         List<Article> articles = articleRepository.findAll();
+
         // Then
         assertThat(articles)
                 .isNotNull()
                 .hasSize(123);
     }
+
     @DisplayName("insert 테스트")
     @Test
     void givenTestData_whenInserting_thenWorksFine() {
         // Given
         long previousCount = articleRepository.count();
-        UserAccount userAccount = userAccountRepository.save(UserAccount.of("newUno", "pw", null, null, null));
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("newKjh", "pw", null, null, null));
         Article article = Article.of(userAccount, "new article", "new content", "#spring");
 
         // When
         articleRepository.save(article);
+
         // Then
         assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
     }
+
     @DisplayName("update 테스트")
     @Test
     void givenTestData_whenUpdating_thenWorksFine() {
@@ -56,11 +72,14 @@ class JpaRepositoryTest {
         Article article = articleRepository.findById(1L).orElseThrow();
         String updatedHashtag = "#springboot";
         article.setHashtag(updatedHashtag);
+
         // When
         Article savedArticle = articleRepository.saveAndFlush(article);
+
         // Then
         assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
     }
+
     @DisplayName("delete 테스트")
     @Test
     void givenTestData_whenDeleting_thenWorksFine() {
@@ -69,10 +88,22 @@ class JpaRepositoryTest {
         long previousArticleCount = articleRepository.count();
         long previousArticleCommentCount = articleCommentRepository.count();
         int deletedCommentsSize = article.getArticleComments().size();
+
         // When
         articleRepository.delete(article);
+
         // Then
         assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
         assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
     }
+
+    @EnableJpaAuditing
+    @TestConfiguration
+    public static class TestJpaConfig {
+        @Bean
+        public AuditorAware<String> auditorAware() {
+            return () -> Optional.of("kjh");
+        }
+    }
+
 }
